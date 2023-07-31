739ddadd7bdff5f090caca05453f017036d25c5c
hoang vu <vumonglep@gmail.com>
2023-07-31 22:54:15 +0700

    init project

---
+++ TinhSum/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// tính tổng từ 1 đến n bất kyd
+
+var s = 0;
+for (int i = 0; i <= 100; i++)
+{
+    s = s + i;
+}
+Console.WriteLine("Tổng là = " + s); // cái chỗ này nỗi 
+Console.WriteLine("Tổng từ 1 đến {0}  là = {1}" , 100, s); // chỗ này là cách viết thay thế {0} là vị trí {0} n
+
+
